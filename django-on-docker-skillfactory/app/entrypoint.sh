405fb14bbf0748835e825992a0691d87efa04ae5
iikoDashboard <85111425+onestepchik@users.noreply.github.com>
2023-12-04 16:18:38 +0200

    Project Enviroment for Skillfactory

---
+++ django-on-docker-skillfactory/app/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python3 manage.py shell
+
+exec "$@"
